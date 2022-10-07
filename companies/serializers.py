d4c4f4c8b267fe40af564c34a6070d0848904607
paulo <paulodgsouza@gmail.com>
2022-10-07 16:30:05 -0300

    :sparkles: add the company balance

--- companies/serializers.py
+++ companies/serializers.py
@@ -1,8 +1,27 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from companies.models import Company
+from transactions.models import TransactionTypes
 
 
 class CompanySerializer(ModelSerializer):
+
+    balance = SerializerMethodField()
+
+    def get_balance(self, obj):
+
+        counter = 0
+
+        for transaction in obj.transactions.all():
+
+            typeIdx = int(transaction.type) - 1
+            transactionValue = int(transaction.value)
+
+            if TransactionTypes[typeIdx][1][-1] == "+":
+                counter = counter + transactionValue
+            else:
+                counter = counter - transactionValue
+
+        return counter/100
 
     class Meta:
         model = Company
--- transactions/models.py
+++ transactions/models.py
@@ -3,22 +3,24 @@ from django.utils.translation import gettext_lazy as _
 from django.db.models import Model, UUIDField, TextChoices, CharField, DateTimeField,  ForeignKey, CASCADE
 
 
+TransactionTypes = [
+    ('1', 'Débito+'),
+    ('2', 'Boleto-'),
+    ('3', 'Financiamento-'),
+    ('4', 'Crédito+'),
+    ('5', 'Recebimento Empréstimo+'),
+    ('6', 'Vendas+'),
+    ('7', 'Recebimento TED+'),
+    ('8', 'Recebimento DOC+'),
+    ('9', 'Aluguel-'),
+]
+
+
 class Transaction(Model):
     id = UUIDField(default=uuid4, unique=True,
                    primary_key=True, editable=False)
 
-    class TransactionTypes(TextChoices):
-        DEBITO = '1', _('Débito+')
-        BOLETO = '2', _('Boleto-')
-        FINANCIAMENTO = '3', _('Financiamento-')
-        CREDITO = '4', _('Crédito+')
-        RECEBIMENTO_EMPRESTIMO = '5', _('Recebimento Empréstimo+')
-        VENDAS = '6', _('Vendas+')
-        RECEBIMENTO_TED = '7', _('Recebimento TED+')
-        RECEBIMENTO_DOC = '8', _('Recebimento DOC+')
-        ALUGUEL = '9', _('Aluguel-')
-
-    type = CharField(max_length=1, choices=TransactionTypes.choices)
+    type = CharField(max_length=1, choices=TransactionTypes)
     value = CharField(max_length=10)
     CPF = CharField(max_length=11)
     card = CharField(max_length=12)
